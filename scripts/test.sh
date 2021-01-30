ab848b300d1df73a49bdb0011df597a2103f16c2
Austin Scola <austinscola@gmail.com>
2021-01-30 10:20:29 -0500

    Add test script
    Add a script for running pytests.

---
+++ scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+HERE="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+SELIGIMUS="$(realpath "${HERE}/..")"
+
+pushd "${SELIGIMUS}" > /dev/null
+trap "popd > /dev/null" EXIT
+
+python3 -m pytest
