7e01690d9dc1c6d7aab9440c362085fc5d5acbe7
Austin Scola <austinscola@gmail.com>
2021-02-01 08:40:52 -0500

    Make readme reference relative
    Make the reference to the README relative to the `setup.py` file.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 """A build script using setuptools for the seligimus package."""
+import pathlib
+
 import setuptools  # type: ignore
 
-with open('README.md', 'r') as readme:
-    long_description = readme.read()
+_HERE = pathlib.Path(__file__).parent
+
+_README = _HERE / 'README.md'
+_LONG_DESCRIPTION = _README.read_text()
 
 setuptools.setup(
     name='seligimus',
@@ -10,7 +14,7 @@ setuptools.setup(
     author='Austin Scola',
     author_email='austinscola@gmail.com',
     description='A mono-library.',
-    long_description=long_description,
+    long_description=_LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     url='https://github.com/pypa/sampleproject',
     packages=['seligimus'],
