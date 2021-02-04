2a4e95c5236ce5d3cb399c1893ed16acc939bebc
Austin Scola <austinscola@gmail.com>
2021-02-04 06:35:34 -0500

    Move buildinv venv
    Move the location of the virtual environment used for building
    distributions into the `venvs` directory. Having one place for all venvs
    will hopefully make them easier to keep track of.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,8 @@ SELIGIMUS="$(realpath "${HERE}/..")"
 pushd "${SELIGIMUS}" > /dev/null
 trap "popd > /dev/null" EXIT
 
-python3 -m venv "${SELIGIMUS}/venv"
-source "${SELIGIMUS}/venv/bin/activate"
+python3 -m venv "${SELIGIMUS}/venvs/distribution_building"
+source "${SELIGIMUS}/venvs/distribution_building/bin/activate"
 python3 -m pip install -r "${SELIGIMUS}/requirements/build_requirements.txt"
 
 python3 setup.py sdist bdist_wheel
