bd69d9fd3806aba82fcca41e7345df834ccd0500
Austin Scola <austinscola@gmail.com>
2021-02-04 06:39:40 -0500

    Define building venv in variable
    Define the location of the distrubution building virtual environment in
    a variable so that it can be re-used and is easier to read.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,10 @@ SELIGIMUS="$(realpath "${HERE}/..")"
 pushd "${SELIGIMUS}" > /dev/null
 trap "popd > /dev/null" EXIT
 
-python3 -m venv "${SELIGIMUS}/venvs/distribution_building"
-source "${SELIGIMUS}/venvs/distribution_building/bin/activate"
+VENV="${SELIGIMUS}/venvs/distribution_building"
+
+python3 -m venv "${VENV}"
+source "${VENV}/bin/activate"
 python3 -m pip install -r "${SELIGIMUS}/requirements/build_requirements.txt"
 
 python3 setup.py sdist bdist_wheel
