b58bca9c72fdd06d26849aaf9d6076a0d8c9366c
Austin Scola <austinscola@gmail.com>
2021-02-08 06:23:48 -0500

    Add staging script
    Add a script for staging the distributions on test PyPI.

---
+++ scripts/stage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+HERE="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+SELIGIMUS="$(realpath "${HERE}/..")"
+DISTRIBUTION_DIRECTORY="${SELIGIMUS}/dist"
+
+VERSION_FILE="${SELIGIMUS}/VERSION.txt"
+VERSION="$(cat "${VERSION_FILE}")"
+
+WHEEL="${DISTRIBUTION_DIRECTORY}/seligimus-${VERSION}-py3-none-any.whl"
+SOURCE_DISTRIBUTION="${DISTRIBUTION_DIRECTORY}/seligimus-${VERSION}.tar.gz"
+
+# Check that the API token has been provided.
+set +u
+if [[ -z "${TEST_PYPI_TOKEN}" ]]; then
+    echo "ERROR: Test PyPI API token not set. Please provide it as an environment varibale."
+    exit 1
+fi
+set -u
+
+# Set up a virtual environment.
+VENV="${SELIGIMUS}/venvs/deployment"
+rm -rf ${VENV}
+python3 -m venv "${VENV}"
+source "${VENV}/bin/activate"
+
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install -r "${SELIGIMUS}/requirements/deployment_requirements.txt"
+
+python3 -m twine upload \
+    --repository testpypi \
+    --non-interactive \
+    --username __token__ \
+    --password "${TEST_PYPI_TOKEN}" \
+    "${WHEEL}" "${SOURCE_DISTRIBUTION}"
