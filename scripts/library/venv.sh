344b1484416384b92570e5416d29763a36a09170
Austin Scola <austinscola@gmail.com>
2021-02-10 06:33:36 -0500

    Use local variables in venv functions
    Use local variables in venv functions so that scripts can't accidentally
    depend on the variables.

--- scripts/library/venv.sh
+++ scripts/library/venv.sh
@@ -3,7 +3,7 @@
 set -eu
 
 function _make_venv() {
-    VENV="$1"
+    local VENV="$1"
     python3 -m venv --clear "${VENV}"
 }
 
@@ -12,8 +12,8 @@ function _activate_venv() {
 }
 
 function _install_requirements() {
-    REQUIREMENTS_FILE="$1"
-    REQUIREMENTS_PATH="${SELIGIMUS}/requirements/${REQUIREMENTS_FILE}"
+    local REQUIREMENTS_FILE="$1"
+    local REQUIREMENTS_PATH="${SELIGIMUS}/requirements/${REQUIREMENTS_FILE}"
     python3 -m pip install -r "${REQUIREMENTS_PATH}"
 }
 
@@ -24,10 +24,10 @@ function get_venv_path() {
 }
 
 function use_venv() {
-    VENV_NAME="$1"
-    REQUIREMENTS_FILE="$2"
+    local VENV_NAME="$1"
+    local REQUIREMENTS_FILE="$2"
 
-    VENV="${SELIGIMUS}/venvs/${VENV_NAME}"
+    local VENV="${SELIGIMUS}/venvs/${VENV_NAME}"
 
     _make_venv "${VENV}"
     _activate_venv "${VENV}"
