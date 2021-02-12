26035fe13faae90b4d3cf5b24f7388e425401056
Austin Scola <austinscola@gmail.com>
2021-02-12 06:56:14 -0500

    Add requirements freezing script
    Add a script to freeze requirements for each requirements file.

---
+++ scripts/freeze.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+HERE="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+SELIGIMUS="$(realpath "${HERE}/..")"
+REQUIREMENTS_DIRECTORY="${SELIGIMUS}/requirements"
+FROZEN_REQUIREMENTS_DIRECTORY="${REQUIREMENTS_DIRECTORY}/frozen"
+
+source "${SELIGIMUS}/scripts/library/string.sh"
+source "${SELIGIMUS}/scripts/library/venv.sh"
+
+
+function freeze_requirements() {
+    local REQUIREMENTS_FILE_PATH="$1"
+    echo "Freezing '${REQUIREMENTS_FILE_PATH}'..."
+
+    local VENV_NAME="$(get_venv_name_from_requirements_file "${REQUIREMENTS_FILE_PATH}")"
+    echo "Using virtual environment name '${VENV_NAME}'."
+
+    local REQUIREMENTS_FILE="$(basename "${REQUIREMENTS_FILE_PATH}")"
+    use_venv "${VENV_NAME}" "${REQUIREMENTS_FILE}"
+
+    local FROZEN_REQUIREMENTS="$(pip freeze)"
+
+    set +u
+    deactivate
+    set -u
+
+    FROZEN_REQUIREMENTS_FILE_NAME="frozen_${REQUIREMENTS_FILE}"
+    FROZEN_REQUIREMENTS_FILE_PATH="${FROZEN_REQUIREMENTS_DIRECTORY}/${FROZEN_REQUIREMENTS_FILE_NAME}"
+
+    touch "${FROZEN_REQUIREMENTS_FILE_PATH}"
+    echo "${FROZEN_REQUIREMENTS}" > "${FROZEN_REQUIREMENTS_FILE_PATH}"
+
+    echo "Done freezing requirements for '${REQUIREMENTS_FILE_PATH}'."
+}
+
+REQUIREMENTS_FILE_PATHS="$(find "${REQUIREMENTS_DIRECTORY}" -maxdepth 1 -name "*_requirements.txt")"
+
+NUMBER_OF_REQUIREMENTS_FILE_PATHS="$(number_of_lines "${REQUIREMENTS_FILE_PATHS}")"
+
+echo "Found ${NUMBER_OF_REQUIREMENTS_FILE_PATHS} requirements files to freeze."
+
+for_each_line "${REQUIREMENTS_FILE_PATHS}" freeze_requirements
