dd7cf66971c36d6044eee3e62dfc7552c9662b15
Austin Scola <austinscola@gmail.com>
2021-02-28 06:37:25 -0500

    Add default values for integer position 2D
    Add default values for x and y of 0 for the integer position 2D.

--- tests/maths/test_integer_position_2d.py
+++ tests/maths/test_integer_position_2d.py
@@ -1,4 +1,8 @@
 """Test seligimus.maths.integer_position_2d."""
+from typing import Any, Dict, List, Optional
+
+import pytest
+
 from seligimus.maths.integer_position_2d import IntegerPosition2D
 from seligimus.maths.vector_2 import Vector2
 from seligimus.python.classes.is_subclass_of_generic import is_subclass_of_generic
@@ -7,3 +11,50 @@ from seligimus.python.classes.is_subclass_of_generic import is_subclass_of_gener
 def test_inheritance() -> None:
     """Test seligimus.maths.integer_position_2d.IntegerPosition2D inheritance."""
     assert is_subclass_of_generic(IntegerPosition2D, Vector2[int])
+
+
+# yapf: disable # pylint: disable=line-too-long
+@pytest.mark.parametrize('x, pass_x_as_argument, pass_x_as_keyword_argument, y, pass_y_as_argument, pass_y_as_keyword_argument, expected_x, expected_y', [
+    (None, False, False, None, False, False, 0, 0),
+    (None, False, False, 0, False, True, 0, 0),
+    (None, False, False, 1, False, True, 0, 1),
+    (0, False, True, None, False, False, 0, 0),
+    (1, False, True, None, False, False, 1, 0),
+    (0, False, True, 0, False, True, 0, 0),
+    (0, False, True, 1, False, True, 0, 1),
+    (1, False, True, 0, False, True, 1, 0),
+    (1, False, True, 1, False, True, 1, 1),
+    (0, True, False, 0, False, True, 0, 0),
+    (0, True, False, 1, False, True, 0, 1),
+    (1, True, False, 0, False, True, 1, 0),
+    (1, True, False, 1, False, True, 1, 1),
+    (0, True, False, 0, True, False, 0, 0),
+    (0, True, False, 1, True, False, 0, 1),
+    (1, True, False, 0, True, False, 1, 0),
+    (1, True, False, 1, True, False, 1, 1),
+])  # pylint: disable=invalid-name
+# yapf: enable # pylint: enable=line-too-long
+# pylint: disable=too-many-arguments
+def test_init(x: Optional[int], pass_x_as_argument: bool, pass_x_as_keyword_argument: bool,
+              y: Optional[int], pass_y_as_argument: bool, pass_y_as_keyword_argument: bool,
+              expected_x: int, expected_y: int) -> None:
+    """Test seligimus.maths.integer_position_2d.IntegerPosition2D.__init__."""
+    arguments: List[Any] = []
+    keyword_arguments: Dict[str, Any] = {}
+
+    if pass_x_as_argument:
+        arguments.append(x)
+
+    if pass_y_as_argument:
+        arguments.append(y)
+
+    if pass_x_as_keyword_argument:
+        keyword_arguments['x'] = x
+
+    if pass_y_as_keyword_argument:
+        keyword_arguments['y'] = y
+
+    integer_position_2d: IntegerPosition2D = IntegerPosition2D(*arguments, **keyword_arguments)
+
+    assert integer_position_2d.x == expected_x
+    assert integer_position_2d.y == expected_y
