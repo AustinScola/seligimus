75c33b0a278f04c611061a8a861c4d767f9e5922
Austin Scola <austinscola@gmail.com>
2021-04-04 10:57:48 -0400

    Add missing periods to docstrings

--- seligimus/characters/lowercase_letters.py
+++ seligimus/characters/lowercase_letters.py
@@ -6,10 +6,10 @@ LOWERCASE_LETTERS: Set[str] = set(string.ascii_lowercase)
 
 
 def is_lowercase_letter(character: str) -> bool:
-    """Return if a character is a lowercase letter"""
+    """Return if a character is a lowercase letter."""
     return character in LOWERCASE_LETTERS
 
 
 def not_lowercase_letter(character: str) -> bool:
-    """Return if a character is not a lowercase letter"""
+    """Return if a character is not a lowercase letter."""
     return character not in LOWERCASE_LETTERS
