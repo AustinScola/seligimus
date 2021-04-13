3cb066e90c887791527bbdf76ae09d79d47fc4eb
Austin Scola <austinscola@gmail.com>
2021-04-13 07:05:08 -0400

    Add excludes to equal instance attribtues
    Add the ability to exclude attributes when comparing the instance
    attributes of two objects to determine the equality of the objects.

--- seligimus/python/decorators/operators/equality/equal_instance_attributes.py
+++ seligimus/python/decorators/operators/equality/equal_instance_attributes.py
@@ -1,23 +1,55 @@
 """A decorator for equality operators to check that the other object has equal instance
 attributes."""
-from functools import wraps
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Set, Union, overload
 
 from seligimus.python.classes.attributes import get_instance_attributes
-from seligimus.python.decorators.operators.equality.equality_operator import EqualityOperator
+from seligimus.python.decorators.operators.equality.equality_operator import (
+    EqualityOperator, EqualityOperatorDecorator)
 
 
-def equal_instance_attributes(equality_operator: EqualityOperator) -> EqualityOperator:
-    """Return an equality operator which first checks if the other object has equal instance
-    attributes."""
-    @wraps(equality_operator)
-    def equality_operator_with_equal_instance_attributes(self: Any, other: Any) -> bool:
-        self_attributes: Dict[str, Any] = get_instance_attributes(self)
-        other_attributes: Dict[str, Any] = get_instance_attributes(other)
+@overload
+def equal_instance_attributes(equality_operator: EqualityOperator,
+                              excludes: None = None) -> EqualityOperator:
+    ...  # pragma: no cover
 
-        if not self_attributes == other_attributes:
-            return False
 
-        return equality_operator(self, other)
+@overload
+def equal_instance_attributes(equality_operator: None = None,
+                              excludes: Set[str] = ...) -> EqualityOperatorDecorator:
+    ...  # pragma: no cover
 
-    return equality_operator_with_equal_instance_attributes
+
+def equal_instance_attributes(
+        equality_operator: Optional[EqualityOperator] = None,
+        excludes: Optional[Set[str]] = None) -> Union[EqualityOperator, EqualityOperatorDecorator]:
+    """Return a decorated equality operator or an equality operator decorator.
+
+    If an equality operator is passed, then return a decorated equality operator. Else If no
+    equality operator is passed, then return an equality operator decorator.
+
+    The decorator returns an equality operator which checks that the objects have the same
+    instance attributes.
+    """
+
+    _excludes: Set[str] = excludes if excludes is not None else set()
+
+    def equality_operator_decorator(equality_operator: EqualityOperator) -> EqualityOperator:
+        def decorated_equality_operator(self: Any, other: Any) -> bool:
+            self_attributes: Dict[str, Any] = get_instance_attributes(self)
+            other_attributes: Dict[str, Any] = get_instance_attributes(other)
+
+            for exclude in _excludes:
+                self_attributes.pop(exclude)
+                other_attributes.pop(exclude)
+
+            if not self_attributes == other_attributes:
+                return False
+
+            return equality_operator(self, other)
+
+        return decorated_equality_operator
+
+    if equality_operator is not None:
+        return equality_operator_decorator(equality_operator)
+
+    return equality_operator_decorator
--- tests/python/decorators/operators/equality/test_equal_instance_attributes.py
+++ tests/python/decorators/operators/equality/test_equal_instance_attributes.py
@@ -1,5 +1,5 @@
 """Test seligimus.python.decorators.operators.equality.equal_instance_attributes."""
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Set
 
 import pytest
 
@@ -7,24 +7,28 @@ from seligimus.python.classes.attributes import set_attributes
 from seligimus.python.classes.empty_class import EmptyClass
 from seligimus.python.decorators.operators.equality.equal_instance_attributes import \
     equal_instance_attributes
-from seligimus.python.decorators.operators.equality.equality_operator import EqualityOperator
+from seligimus.python.decorators.operators.equality.equality_operator import (
+    EqualityOperator, EqualityOperatorDecorator)
 
 
 # yapf: disable # pylint: disable=line-too-long
-@pytest.mark.parametrize('equality_operator, self_attributes, other_attributes, expected_equality', [
-    (lambda self, other: False, {}, {}, False),
-    (lambda self, other: True, {'foo': 1}, {}, False),
-    (lambda self, other: True, {'foo': 1}, {'foo': 2}, False),
-    (lambda self, other: True, {'foo': 1}, {'bar': 1}, False),
-    (lambda self, other: True, {'foo': 1}, {'bar': 2}, False),
-    (lambda self, other: True, {'foo': 1, 'bar': 2}, {'bar': 2}, False),
-    (lambda self, other: True, {}, {}, True),
-    (lambda self, other: True, {'foo': 1}, {'foo': 1}, True),
-    (lambda self, other: True, {'foo': 1, 'bar': 2}, {'foo': 1, 'bar': 2}, True),
+@pytest.mark.parametrize('equality_operator, excludes, self_attributes, other_attributes, expected_equality', [
+    (lambda self, other: False, None, {}, {}, False),
+    (lambda self, other: True, None, {'foo': 1}, {}, False),
+    (lambda self, other: True, None, {'foo': 1}, {'foo': 2}, False),
+    (lambda self, other: True, None, {'foo': 1}, {'bar': 1}, False),
+    (lambda self, other: True, None, {'foo': 1}, {'bar': 2}, False),
+    (lambda self, other: True, None, {'foo': 1, 'bar': 2}, {'bar': 2}, False),
+    (lambda self, other: True, None, {}, {}, True),
+    (lambda self, other: True, None, {'foo': 1}, {'foo': 1}, True),
+    (lambda self, other: True, None, {'foo': 1, 'bar': 2}, {'foo': 1, 'bar': 2}, True),
+    (lambda self, other: True, set(), {}, {}, True),
+    (lambda self, other: True, {'foo'}, {'foo': 1}, {'foo': 2}, True),
+    (lambda self, other: True, {'bar'}, {'foo': 1, 'bar': 2}, {'foo': 1, 'bar': 7}, True),
 ])
 # yapf: enable # pylint: enable=line-too-long
-def test_equal_instance_attributes(equality_operator: EqualityOperator, self_attributes: Dict[str,
-                                                                                              Any],
+def test_equal_instance_attributes(equality_operator: EqualityOperator,
+                                   excludes: Optional[Set[str]], self_attributes: Dict[str, Any],
                                    other_attributes: Dict[str,
                                                           Any], expected_equality: bool) -> None:
     """Test seligimus.python.decorators.operators.equality.equal_instance_attributes.equal_instance_attributes."""  # pylint: disable=line-too-long, useless-suppression
@@ -34,8 +38,13 @@ def test_equal_instance_attributes(equality_operator: EqualityOperator, self_att
     other = EmptyClass()
     set_attributes(other, other_attributes)
 
-    wrapped_equality_operator = equal_instance_attributes(equality_operator)
+    equality_operator_decorator: EqualityOperatorDecorator
+    if excludes is not None:
+        equality_operator_decorator = equal_instance_attributes(excludes=excludes)
+    else:
+        equality_operator_decorator = equal_instance_attributes
 
-    equality: bool = wrapped_equality_operator(self, other)
+    decorated_equality_operator = equality_operator_decorator(equality_operator)
+    equality: bool = decorated_equality_operator(self, other)
 
     assert equality == expected_equality
